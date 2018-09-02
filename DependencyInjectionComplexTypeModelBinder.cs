cb2bb7c05318403b12a9df15512c58d35c51a333
Alexander Powolozki <alexander@powolozki.de>
2018-09-03 01:09:06 +0200

    - initial version 1.0

---
+++ DependencyInjectionComplexTypeModelBinder.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+
+namespace De.Powolozki.AspNetCore.ModelBinding
+{
+    public class DependencyInjectionComplexTypeModelBinder : ComplexTypeModelBinder
+    {
+        public DependencyInjectionComplexTypeModelBinder(
+            IDictionary<ModelMetadata, IModelBinder> propertyBinders,
+            ILoggerFactory loggerFactory) : base(propertyBinders, loggerFactory) { }
+
+        protected override object CreateModel(ModelBindingContext bindingContext)
+        {
+            var modelFromService = bindingContext?.HttpContext?.RequestServices?.GetService(bindingContext.ModelType);
+
+            return modelFromService ?? base.CreateModel(bindingContext);
+        }
+    }
+}
---
+++ DependencyInjectionComplexTypeModelBinderProvider.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Linq;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace De.Powolozki.AspNetCore.ModelBinding
+{
+    public class DependencyInjectionComplexTypeModelBinderProvider : IModelBinderProvider
+    {
+        IModelBinder IModelBinderProvider.GetBinder(ModelBinderProviderContext context)
+        {
+            if (context == null) { throw new ArgumentNullException(nameof(context)); }
+
+
+            if (context.Metadata.IsComplexType && !context.Metadata.IsCollectionType
+                && context.Services.GetService(context.Metadata.ModelType) != null)
+            {
+
+                var propertyBinders = context.Metadata.Properties
+                    .Select(prop => new { Key = prop, Value = context.CreateBinder(prop) })
+                    .ToDictionary(q => q.Key, q => q.Value);
+
+                var loggerFactory = context.Services.GetService<ILoggerFactory>();
+                return new DependencyInjectionComplexTypeModelBinder(propertyBinders, loggerFactory);
+            }
+
+            return null;
+        }
+    }
+}
